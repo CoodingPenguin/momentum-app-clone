aa339f5e5c8f49d1e3bbabf417c8bd8ee217e46d
CoodingPenguin <unodostre@naver.com>
2020-02-03 11:55:59 +0900

    Vanilla JS Challenge Day7

--- homework/hw2_sol/src/index.js
+++ homework/hw2_sol/src/index.js
@@ -2,6 +2,8 @@ import "./styles.css";
 
 const body = document.body;
 
+// class name : 상태 별로 class name만 바꿔준다.
+// 그래서 class name별로 다른 css가 적용되게 한다.
 const BIG_SCREEN = "bigScreen";
 const MEDIUM_SCREEN = "mediumScreen";
 const SMALL_SCREEN = "smallScreen";
